8046187c4d71fb652c6161ca231499dbebe6ca66
RAHUL-PC\abhik <rahul.verlekar@bidchat.com>
2016-08-18 15:38:16 +0530

    Added the code for the following Edits
    StopCamera(OnSucess) //Added the callback for the function
    TakePicture(Size, callback) //Added callbacks to the code
    OnTakePictureHandler() //returns a Data stream instead of a JSON
    startCamera()//Done changed the funtion so that it take every argument including the OnSuccess and OnError
    hide(onSucess, Onerror) //Added the Success and the faliure Callbacks
    show(onSucess, Onerror) //Added the Success and the faliure Callbacks

--- www/CameraPreview.js
+++ www/CameraPreview.js
@@ -18,46 +18,45 @@ CameraPreview.setOnLogHandler = function(onLog) {
   exec(onLog, onLog, PLUGIN_NAME, "wLog", []);
 };
 
-CameraPreview.startCamera = function(options){
-  options = options || {};
-  if(typeof(options.x) === 'undefined'){
-    options.x = 0;
+CameraPreview.startCamera = function(rect, defaultCamera, tapEnabled, dragEnabled, toBack, alpha, onSuccess, onError){
+  if(typeof(rect.x) === 'undefined'){
+    rect.x = 0;
   }
-  if(typeof(options.y) === 'undefined'){
-    options.y = 0;
+  if(typeof(rect.y) === 'undefined'){
+    rect.y = 0;
   }
-  if(typeof(options.width) === 'undefined'){
-    options.width = window.screen.width;
+  if(typeof(rect.width) === 'undefined'){
+    rect.width = window.screen.width;
   }
-  if(typeof(options.height) === 'undefined'){
-    options.height = window.screen.height;
+  if(typeof(rect.height) === 'undefined'){
+    rect.height = window.screen.height;
   }
-  if(typeof(options.camera) === 'undefined'){
-    options.camera = 'front';
+  if(typeof(defaultCamera) === 'undefined'){
+    defaultCamera = 'front';
   }
-  if(typeof(options.tapPhoto) === 'undefined'){
-    options.tapPhoto = true;
+  if(typeof(tapEnabled) === 'undefined'){
+    tapEnabled = true;
   }
-  if(typeof(options.previewDrag) === 'undefined'){
-    options.previewDrag = false;
+  if(typeof(dragEnabled) === 'undefined'){
+    dragEnabled = false;
   }
-  if(typeof(options.toBack) === 'undefined'){
-    options.toBack = false;
+  if(typeof(toBack) === 'undefined'){
+    toBack = false;
   }
-  if(typeof(options.alpha) === 'undefined'){
-    options.alpha = 1;
+  if(typeof(alpha) === 'undefined'){
+    alpha = 1;
   }
 
-  exec(null, null, PLUGIN_NAME, "startCamera", [options.x, options.y, options.width, options.height, options.camera, options.tapPhoto, options.previewDrag, options.toBack, options.alpha]);
+  exec(onSuccess, onError, PLUGIN_NAME, "startCamera", [rect.x, rect.y, rect.width, rect.height, defaultCamera, tapEnabled, dragEnabled, toBack, alpha]);
 };
 
-CameraPreview.stopCamera = function(){
-  exec(null, null, PLUGIN_NAME, "stopCamera", []);
+CameraPreview.stopCamera = function(callback){
+  exec(callback, callback, PLUGIN_NAME, "stopCamera", []);
 };
 
-CameraPreview.takePicture = function(dim){
+CameraPreview.takePicture = function(dim, onSuccess, onError){
   dim = dim || {};
-  exec(null, null, PLUGIN_NAME, "takePicture", [dim.maxWidth || 0, dim.maxHeight || 0]);
+  exec(onSuccess, onError, PLUGIN_NAME, "takePicture", [dim.maxWidth || 0, dim.maxHeight || 0]);
 };
 
 CameraPreview.setColorEffect = function(effect){
@@ -68,12 +67,12 @@ CameraPreview.switchCamera = function(){
   exec(null, null, PLUGIN_NAME, "switchCamera", []);
 };
 
-CameraPreview.hide = function(){
-  exec(null, null, PLUGIN_NAME, "hideCamera", []);
+CameraPreview.hide = function(onSuccess, onError){
+  exec(onSuccess, onError, PLUGIN_NAME, "hideCamera", []);
 };
 
-CameraPreview.show = function(){
-  exec(null, null, PLUGIN_NAME, "showCamera", []);
+CameraPreview.show = function(onSuccess, onError){
+  exec(onSuccess, onError, PLUGIN_NAME, "showCamera", []);
 };
 
 CameraPreview.disable = function(disable){
